aa847cb5f527b17ba7f503b7491d4f8fb0023665
Oğuzhan Yamacı <oguzhanyamaci175@gmail.com>
2023-09-16 15:11:22 +0300

    models

---
+++ Controllers/AssignmentsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LearningApp.Data;
+using LearningApp.Models;
+
+namespace LearningApp.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        private readonly LearningAppContext _context;
+
+        public AssignmentsController(LearningAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Assignments
+        public async Task<IActionResult> Index()
+        {
+              return _context.Assignments != null ? 
+                          View(await _context.Assignments.ToListAsync()) :
+                          Problem("Entity set 'LearningAppContext.Assignments'  is null.");
+        }
+
+        // GET: Assignments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Assignments == null)
+            {
+                return NotFound();
+            }
+
+            var assignments = await _context.Assignments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (assignments == null)
+            {
+                return NotFound();
+            }
+
+            return View(assignments);
+        }
+
+        // GET: Assignments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Assignments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CourseId,Title,Descriptions,DueDate")] Assignments assignments)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(assignments);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(assignments);
+        }
+
+        // GET: Assignments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Assignments == null)
+            {
+                return NotFound();
+            }
+
+            var assignments = await _context.Assignments.FindAsync(id);
+            if (assignments == null)
+            {
+                return NotFound();
+            }
+            return View(assignments);
+        }
+
+        // POST: Assignments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CourseId,Title,Description,DueDate")] Assignments assignments)
+        {
+            if (id != assignments.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(assignments);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AssignmentsExists(assignments.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(assignments);
+        }
+
+        // GET: Assignments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Assignments == null)
+            {
+                return NotFound();
+            }
+
+            var assignments = await _context.Assignments
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (assignments == null)
+            {
+                return NotFound();
+            }
+
+            return View(assignments);
+        }
+
+        // POST: Assignments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Assignments == null)
+            {
+                return Problem("Entity set 'LearningAppContext.Assignments'  is null.");
+            }
+            var assignments = await _context.Assignments.FindAsync(id);
+            if (assignments != null)
+            {
+                _context.Assignments.Remove(assignments);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AssignmentsExists(int id)
+        {
+          return (_context.Assignments?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LearningApp.Data;
+using LearningApp.Models;
+
+namespace LearningApp.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly LearningAppContext _context;
+
+        public CoursesController(LearningAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Courses
+        public async Task<IActionResult> Index()
+        {
+              return _context.Courses != null ? 
+                          View(await _context.Courses.ToListAsync()) :
+                          Problem("Entity set 'LearningAppContext.Courses'  is null.");
+        }
+
+        // GET: Courses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Courses == null)
+            {
+                return NotFound();
+            }
+
+            var courses = await _context.Courses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (courses == null)
+            {
+                return NotFound();
+            }
+
+            return View(courses);
+        }
+
+        // GET: Courses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Courses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,InstructorId,Category,EnrollmentCount,ImageUrl")] Courses courses)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(courses);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(courses);
+        }
+
+        // GET: Courses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Courses == null)
+            {
+                return NotFound();
+            }
+
+            var courses = await _context.Courses.FindAsync(id);
+            if (courses == null)
+            {
+                return NotFound();
+            }
+            return View(courses);
+        }
+
+        // POST: Courses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,InstructorId,Category,EnrollmentCount,ImageUrl")] Courses courses)
+        {
+            if (id != courses.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(courses);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CoursesExists(courses.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(courses);
+        }
+
+        // GET: Courses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Courses == null)
+            {
+                return NotFound();
+            }
+
+            var courses = await _context.Courses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (courses == null)
+            {
+                return NotFound();
+            }
+
+            return View(courses);
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Courses == null)
+            {
+                return Problem("Entity set 'LearningAppContext.Courses'  is null.");
+            }
+            var courses = await _context.Courses.FindAsync(id);
+            if (courses != null)
+            {
+                _context.Courses.Remove(courses);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CoursesExists(int id)
+        {
+          return (_context.Courses?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
