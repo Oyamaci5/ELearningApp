0c2d6ab3928f6931a4f744e21c29ff171f5db325
OkcuYazilimci <umutuygun.u@gmail.com>
2023-09-20 00:16:04 +0300

    umut

--- Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
+++ Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -1,23 +1,23 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+// "Nullable reference types" will not be used in this code block
 #nullable disable
 
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace elearningapp.Areas.Identity.Pages.Account
 {
-    /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
     ///     directly from your code. This API may change or be removed in future releases.
-    /// </summary>
     public class AccessDeniedModel : PageModel
     {
         /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
+        /// This method handles HTTP GET requests to the Access Denied page.
         /// </summary>
         public void OnGet()
         {
+            // This method currently doesn't have any specific functionality.
+            // It is intended to respond to GET requests for the Access Denied page.
         }
     }
 }
