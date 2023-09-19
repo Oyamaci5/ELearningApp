121d6daf13e1fc1c7284fa2c12c70faa073553c0
sevil <sevil.oksuz@std.yildiz.edu.tr>
2023-09-19 12:18:45 +0300

    courses page

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -57,7 +57,7 @@ namespace elearningapp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Description,InstructorId,Category,EnrollmentCount,ImageUrl")] Courses courses)
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,InstructorId,Category,EnrollmentCount,ImageUrl,CourseDuration")] Courses courses)
         {
             if (ModelState.IsValid)
             {
@@ -90,7 +90,7 @@ namespace elearningapp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,InstructorId,Category,EnrollmentCount,ImageUrl")] Courses courses)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,InstructorId,Category,EnrollmentCount,ImageUrl,CourseDuration")] Courses courses)
         {
             if (id != courses.Id)
             {
